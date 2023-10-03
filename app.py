4880fb17607500a0d9bf20b7c61f8a2a175503c9
Daniel Brito <danielbrito@hotmail.es>
2023-10-03 11:14:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -105,9 +105,9 @@ health = HealthCheck()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
-    searches = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    searches = db.Column(db.String(100), nullable=False)
 
 
 @app.route("/")
