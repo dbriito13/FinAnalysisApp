f64c673ebe95d5a5b347f0328170ae987752178c
Daniel <danielbrito@LAPTOP-GTDHAFVI>
2023-10-03 10:19:27 +0200

    Extended password in User model

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ health = HealthCheck()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
     searches = db.Column(db.String(50), nullable=False)
 
 
