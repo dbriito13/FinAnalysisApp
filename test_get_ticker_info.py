84dcfae87724086f7ddfe05fe9433a274b850340
Daniel <danielbrito@LAPTOP-GTDHAFVI>
2023-10-04 16:07:19 +0200

    Final Version

--- test_get_ticker_info.py
+++ test_get_ticker_info.py
@@ -7,19 +7,16 @@ import pandas as pd
 class TestTickerInfo(unittest.TestCase):
     def setUp(self) -> None:
         self.tickerFetcher = get_ticker_info.TickerFetcher("TIKR")
-        self.url = "https://query1.finance.yahoo.com/v7/finance"
-        f"/download/{self.tickerFetcher}?period1=1030060800&"
-        "period2=1684368000&interval=1d&events=history"
-        "&includeAdjustedClose=true"
 
     @patch('requests.get')
     def test_fetch_prices(self, mocked_get):
         mock_response = Mock()
-        mocked_get.return_value = mock_response
         mock_response.text = ("Date,Open,High,Low,Close,Adj Close,Volume\n"
                               "2002-08-23,0.283929,0.284464,0.275893,"
                               "0.280893,0.238421,163245600")
-
+        mock_response.status_code = 200
+        mocked_get.return_value = mock_response
+        
         expected = pd.DataFrame([['2002-08-23', '0.283929', '0.284464',
                                   '0.275893', '0.280893',
                                   '0.238421', '163245600']],
